84353b4cb53d331ac4c7e45715498447b9708b5f
Abraham E. Tavarez <abetavarez@icloud.com>
2022-09-23 18:41:31 -0400

    update profile init and added to navigation

---
+++ src/screens/UpdateProfileScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const UpdateProfileScreen = () => {
+  return (
+    <View>
+      <Text>Update profile</Text>
+    </View>
+  );
+};
+
+export default UpdateProfileScreen;
