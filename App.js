a46d692a460f62289f6fe91879bce95cb5451898
Abraham E. Tavarez <abetavarez@icloud.com>
2022-09-23 23:19:10 -0400

    amplify added to project

--- App.js
+++ App.js
@@ -2,6 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Navigator from './src/navigation';
 
+// ===== AWS Configuration
+import { Amplify, Auth } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+import { withAuthenticator } from 'aws-amplify-react-native';
+
+Amplify.configure(awsconfig);
+//Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
+
 const App = () => {
   return (
     <View style={styles.container}>
@@ -17,4 +25,7 @@ const styles = StyleSheet.create({
   }
 });
 
-export default App;
+export default withAuthenticator(App);
+//TODO: customize signin and signup screen,
+// 1. using customization in the withAuthenticator func
+// 2. creating custom screen and using Auth from aws-amplify-react-native
